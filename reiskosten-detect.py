1f410f8e65af8201d625a9a676bc8362585a19f3
Nick Smit <nicksmit1994@gmail.com>
2021-12-01 13:44:10 +0100

    Fixed month being 0 when day is beyond 19th of the month in November

--- reiskosten-detect.py
+++ reiskosten-detect.py
@@ -43,13 +43,15 @@ if __name__ == '__main__':
 
         # after 20th should be next month
         month = today.month
+        month = 11
         year = today.year
-        if today.day > config.send_on_day_of_month:
+        if True or today.day > config.send_on_day_of_month:
             if month == 12:
                 # add 1 for december case.
                 year += 1
-            month = (month + 1) % 12
-
+                month = 1
+            else:
+                month += 1
         
         curFile = f"{config.base_dir}data/{today.year}-{month}.json"
         data = []
